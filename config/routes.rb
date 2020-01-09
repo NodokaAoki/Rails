c22b1576c010ff6c7cd932ea16c95b4851f56e04
NodokaAoki <genta91nya@gmail.com>
2020-01-09 17:55:40 +0900

    [Finish]3-6

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
  get 'top'=>'homes#top'
  post 'todolists'=>'todolists#create'
 
- get 'todolisats'=>'todolisats#index'
+ get 'todolists'=>'todolists#index'
+ get 'todolists/:id'=>'todolists#show',as:'todolist'
 end
+
