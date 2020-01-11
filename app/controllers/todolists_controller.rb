69cd4688d5fee60f82094d9c4f1d33e6d7a09a87
NodokaAoki <genta91nya@gmail.com>
2020-01-11 16:36:43 +0900

    [Finish]3-9

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -30,8 +30,11 @@ class TodolistsController < ApplicationController
   	list.update(list_params)
   	redirect_to todolist_path(list.id)
   end
+  #投稿削除
   def destroy
-  	
+  	list=List.find(params[:id])
+  	list.destroy
+  	redirect_to todolists_path
   end
 
   private
