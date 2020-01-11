0ccccd8fe6cc92a65a12ce39323e2a64d4de42cc
NodokaAoki <genta91nya@gmail.com>
2020-01-11 16:36:54 +0900

    [Finish]3-9

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,13 @@ Rails.application.routes.draw do
  get 'top'=>'homes#top'
  post 'todolists'=>'todolists#create'
  #　/todolistsというURLにpostメソッドでリクエストしたとき、
- #　todolistコントローラーのcraateアクションが呼び出される
+ #　todolistコントローラーのcreateアクションが呼び出される
 
  get 'todolists'=>'todolists#index'
  get 'todolists/:id'=>'todolists#show',as:'todolist'
  #️ 'todlists#show'の設定をtodolistとして利用出来る
  get 'todolists/:id/edit'=>'todolists#edit', as:'edit_todolist'
  patch 'todolists/:id'=>'todolists#update', as:'update_todolist'
- delete 'todolists/id'=> 'todolists#destroy',as:'destroy_todolist'
+ delete 'todolists/:id'=> 'todolists#destroy',as:'destroy_todolist'
 end
 
